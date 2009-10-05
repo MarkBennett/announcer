2e70b68756211817adb2fb999181c8a3246288fe
Mark <mark.f.bennett@gmail.com>
2009-10-05 19:05:15 +1100

    Slowly building out a simple interface I'd want to use.  Feel like the way you add venues is too complicated, but I'll go with it for now.

--- lib/announcer.rb
+++ lib/announcer.rb
@@ -1,22 +1,76 @@
-module Announcer
+module Adman
 
   # Indicates this service is a web application
   WEB_APPLICATION = "_web_app"
 
-  class Announcer
+  # Announces services on a network or other venue.
+  class Campaign
 
-    attr_reader :service
+    attr_reader :message
+    attr_reader :venues
 
-    def initialize(service_name)
-      @service = Service.new(service_name)
+    # Create a new Campaign to advertize the given service.
+    #
+    # If the service given isn't already a Service (a String perhaps?) then the
+    # Campaign will try to turn it into one.
+    def initialize(service)
+      # If this isn't yet a service try to turn it into one
+      unless service.kind_of?(Service)
+        service = Service.new(service)
+      end
+      @message = service
+
+      @venues = []
     end
 
+    def add_venue(venue)
+      @venues << venue
+      self
+    end
+
+    # Start advertising this service
+    def launch
+      @venues.each do |venue|
+        venue.register(message)
+      end
+      self
+    end
   end
 
+  # A Service which is announced by the announcer.
+  #
+  # A service is a general term and can be a web application,
+  # mail server, or anything that can described in terms of a
+  # service name, type, and port.
   class Service
+
+    DEFAULT_PORT = 3000
+    DEFAULT_TYPE = "_web_app"
+
+    include Comparable
+
     attr_reader :name
     attr_reader :port
     attr_reader :type
+
+    # Compare this Service to another
+    def <=>(other)
+      if self.name < other.name
+        return -1
+      elsif self.name > other.name
+        return 1
+      elsif self.port < other.port
+        return -1
+      elsif self.port > other.port
+        return 1
+      elsif self.type < other.type
+        return -1
+      elsif self.type > other.type
+        return 1
+      else
+        return 0
+      end
+    end
 
     def initialize(service_name)
       @name = service_name
--- test/announcer_test.rb
+++ test/announcer_test.rb
@@ -1,25 +1,53 @@
 require 'test_helper'
 
+class TestVenue
+  attr_reader :messages
+
+  def initialize
+    @messages = []
+  end
+
+  def register(message)
+    @messages << message
+  end
+end
+
 class AnnouncerTest < Test::Unit::TestCase
-  context "When an Announcer announces a service, it" do
+  context "When a new Campaign is launched for a service, the campaign" do
     setup do
-      @service_name = "My really cool webapp"
-      @announcer = Announcer::announce(@service_name)
-#      @announcer = Announcer::announce do |announcer|
-#        announcer.service(@service_name)
-#      end
+      @service_name = "ShinyApp"
+      @venue = TestVenue.new
+      @campaign = Adman::Campaign.new(@service_name).add_venue(@venue).launch
     end
 
-    should "announce the service to the network" do
-      assert_equal @announcer.service.name, @service_name
+    should "deliver the right message about the right service" do
+      assert_equal @campaign.message, Adman::Service.new(@service_name)
     end
 
-    should "default to announcing the service on port 3000" do
-      assert_equal @announcer.service.port, 3000
+    should "deliver the message to each venue" do
+      assert @campaign.venues.include?(@venue)
+      @campaign.venues.each do |venue|
+        assert venue.messages.include?(@campaign.message)
+      end
+    end
+  end
+
+  context "When a new Service is created from just a String, it" do
+    setup do
+      @service_name = "ShinyApp"
+      @service = Adman::Service.new(@service_name)
     end
 
-    should "default to announcing a web site" do
-      assert_equal @announcer.service.type, Announcer::WEB_APPLICATION
+    should "use the String as the service name" do
+      assert_equal @service.name, @service_name
+    end
+
+    should "use the default port (#{Adman::Service::DEFAULT_PORT})" do
+      assert_equal @service.port, Adman::Service::DEFAULT_PORT
+    end
+
+    should "use the default service type (#{Adman::Service::DEFAULT_TYPE})" do
+      assert_equal @service.type, Adman::Service::DEFAULT_TYPE
     end
   end
 end
